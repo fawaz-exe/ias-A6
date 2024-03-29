af3c2ed41fbac7b886c3ee8ad52f2b170931d216
fawaz-exe <fawaz.exe@gmail.com>
2024-03-29 22:22:47 +0530

    urls,views,setup

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-phi*52^l#mizs(_y9yypu$oeg3_6x26%uh!2*s$bxewfae(&y8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +79,14 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'admin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
+
+
     }
 }
 
@@ -116,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ORIGIN_ALLOWALL = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ admin/products/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def delete(self, request):
+        pass
