7a12db3ad691f15e022127e3545d5abc38348714
fawaz-exe <fawaz.exe@gmail.com>
2024-04-02 20:03:06 +0530

    Docker not running

--- main/main.py
+++ main/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql:root:root@db/main'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
 CORS(app)
 
 db = SQLAlchemy(app)
@@ -15,11 +16,15 @@ class Product(db.Model):
     image = db.Column(db.String(200))
 
 class ProductUser(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 @app.route('/')
 def index():
     return 'Hello'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
